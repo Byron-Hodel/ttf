3231729ab3528fffd4285c376d60df34c6ddb289
Byron Hodel <byron@hodel.com>
2026-03-01 08:00:04 -0500

    modified base types and arena code

--- code/base.h
+++ code/base.h
@@ -18,13 +18,15 @@ typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
-typedef intptr_t isize;
 
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef uintptr_t usize;
+typedef size_t usize;
+
+typedef intptr_t  intptr;
+typedef uintptr_t uintptr;
 
 typedef float f32;
 typedef double f64;
--- code/mem.c
+++ code/mem.c
@@ -13,6 +13,8 @@ global const usize KiB = 1024;
 global const usize MiB = 1024*1024;
 global const usize GiB = 1024*1024*1024;
 
+global Arena scratch_arenas[2];
+
 function void arena_init(Arena *arena, u8 *mem, usize mem_size) {
 	ASSERT(arena != 0);
 	arena->base = mem;
@@ -24,18 +26,14 @@ function u8 *_arena_push(Arena *arena, usize size, usize alignment, b32 zero) {
 	ASSERT(arena != 0);
 	ASSERT(IS_POW2(alignment));
 
-	u8 *mem = 0;
-	if (size > 0) {
-		u8 *aligned_mem = (u8*)ALIGN_FORWARD_POW2((usize)arena->base + arena->pos, alignment);
-		usize next_pos = (usize)aligned_mem - (usize)arena->base + arena->pos + size;
+	u8 *mem = (u8*)ALIGN_FORWARD_POW2((uintptr)arena->base + arena->pos, alignment);
+	usize next_pos = (uintptr)mem  - (uintptr)arena->base + arena->pos + size;
 
-		if (next_pos > arena->size) {
-			debug_trap();
-		}
-
-		arena->pos = next_pos;
-		mem = aligned_mem;
+	if (next_pos > arena->size) {
+		debug_trap();
 	}
+
+	arena->pos = next_pos;
 
 	if (zero) {
 		for (usize i = 0; i < size; i += 1) {
@@ -47,6 +45,9 @@ function u8 *_arena_push(Arena *arena, usize size, usize alignment, b32 zero) {
 
 #define push_struct(arena, T) _arena_push(arena, sizeof(T), alignof(T), TRUE)
 #define push_struct_nz(arena, T) _arena_push(arena, sizeof(T), alignof(T), FALSE)
+
+#define push_struct_no_alignment(arena, T) _arena_push(arena, siweof(T), 1, TRUE)
+#define push_struct_no_alignment_nz(arena, T) _arena_push(arena, sizeof(T), 1, FALSE)
 
 #define push_array(arena, T, n) _arena_push(arena, sizeof(T)*(n), alignof(T), TRUE)
 #define push_array_nz(arena, T, n) _arena_push(arena, sizeof(T)*(n), alignof(T), FALSE)
@@ -74,3 +75,23 @@ function void arena_end_temp(Arena_Temp temp) {
 		arena_pop_to(temp.arena, temp.pos);
 	}
 }
+
+function Arena_Temp begin_scratch(Arena **conflicts, usize num_conflicts) {
+	ASSERT(conflicts == 0 && num_conflicts == 0 || conflicts != 0);
+
+	Arena *scratch_arena = 0;
+	for (usize scratch_index = 0; sizeof(scratch_arenas) / sizeof(Arena); scratch_index += 1) {
+		scratch_arena = &scratch_arenas[scratch_index];
+		for (usize conflict_index = 0; conflict_index < num_conflicts; conflict_index += 1) {
+			if (scratch_arena == conflicts[conflict_index]) {
+				scratch_arena = 0;
+				break;
+			}
+		}
+		if (scratch_arena != 0) {
+			break;
+		}
+	}
+	return arena_begin_temp(scratch_arena);
+}
+#define end_scratch(scratch) arena_end_temp(scratch)
