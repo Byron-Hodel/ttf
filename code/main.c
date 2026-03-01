d6457670a17e1d3e9cd86924b9bde52ce4114291
Byron Hodel <byron@hodel.com>
2026-03-01 08:00:52 -0500

    removed temp arena global and functions from main

--- code/main.c
+++ code/main.c
@@ -4,29 +4,6 @@
 
 #include "base.h"
 #include "mem.c"
-
-global Arena scratch_arenas[2];
-
-function Arena_Temp begin_scratch(Arena **conflicts, usize num_conflicts) {
-	ASSERT(conflicts == 0 && num_conflicts == 0 || conflicts != 0);
-
-	Arena *scratch_arena = 0;
-	for (usize scratch_index = 0; sizeof(scratch_arenas) / sizeof(Arena); scratch_index += 1) {
-		scratch_arena = &scratch_arenas[scratch_index];
-		for (usize conflict_index = 0; conflict_index < num_conflicts; conflict_index += 1) {
-			if (scratch_arena == conflicts[conflict_index]) {
-				scratch_arena = 0;
-				break;
-			}
-		}
-		if (scratch_arena != 0) {
-			break;
-		}
-	}
-	return arena_begin_temp(scratch_arena);
-}
-#define end_scratch(scratch) arena_end_temp(scratch)
-
 #include "ttf.c"
 
 int main(void) {
@@ -48,14 +25,13 @@ int main(void) {
 		fseek(font_file, 0, SEEK_END);
 		font_data_size = (usize)ftell(font_file);
 		fseek(font_file, 0, SEEK_SET);
-		ASSERT(font_data_size <= 0xFFFFFFFF);
 
 		font_data = push_array(scratch.arena, u8, font_data_size);
 		fread(font_data, 1, font_data_size, font_file);
 		fclose(font_file);
 	}
 	
-	Font font = font_make(&arena, font_data, (u32)font_data_size);
+	Font font = font_make(&arena, font_data, font_data_size);
 	end_scratch(scratch);
 
 	u8 lookup_char = 'E';
