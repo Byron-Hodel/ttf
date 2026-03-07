86f4d54a1400334de321a9516227d41d430973af
Byron Hodel <byron@hodel.com>
2026-03-06 22:21:15 -0500

    added raylib 5.5 as a dependency

---
+++ deps_build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -n "$SUDO_USER" ]; then
+	echo "This script must not be run with sudo."
+	exit 1
+fi
+
+set -eu
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd deps
+
+# ensure clean install dir
+rm -rf install
+mkdir install
+
+# build raylib 5.5
+cd raylib-5.5
+
+cmake -S . -B build \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$(pwd)/../install"
+
+cmake --build build
+cmake --install build
+
+cd ..
